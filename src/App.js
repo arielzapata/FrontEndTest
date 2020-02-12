6211a727bd5e4937c01f66870e0e2be873a22a3b
ariel zapata <superarichaka@hotmail.com>
2020-02-12 13:43:45 -0600

    Test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CoverFace from './Face'
+
+function App() {
+  return (
+    <div className="App">
+      <CoverFace />
+    </div>
+  );
+}
+
+export default App;
